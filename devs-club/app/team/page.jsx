6238468b2813026f2ddd54d33feecbf343e8dc5f
TELAPROLU LAKSHMI SRINIVAS <109592965+srinivastls@users.noreply.github.com>
2025-01-11 15:08:25 +0530

    Update page.jsx
    updated wrong roll numbers

--- devs-club/app/team/page.jsx
+++ devs-club/app/team/page.jsx
@@ -268,8 +268,8 @@ export default function Team() {
   }
 
   const cores = [
-    { name: "Vishnu Teja", role: "CS21B2017", bio: "Interested in coding, collaboration, and driving innovation through community-driven development initiatives.", image: '/assets/cs21b2027.jpg', linkedin: "https://www.linkedin.com/in/vishnu-surla-70384524a/", github: "https://github.com/Vishnuteja-Surla" },
     { name: "Harsha Vardhan G", role: "CS21B1052", bio: "Passionate AI enthusiast with research experience and strong technical skills.", image: "/assets/cs21b1052.jpg", linkedin: "https://www.linkedin.com/in/govindharshavardhan", github: "https://github.com/harshavardhan784" },
+    { name: "Vishnu Teja", role: "CS21B2037", bio: "Interested in coding, collaboration, and driving innovation through community-driven development initiatives.", image: '/assets/cs21b2027.jpg', linkedin: "https://www.linkedin.com/in/vishnu-surla-70384524a/", github: "https://github.com/Vishnuteja-Surla" },
     { name: "K M V R Madhava Krishna", role: "CS22B1005", bio: "Data science student excelling in ML, Python, cloud integration, problem-solving, and impactful solutions.", image: "/assets/cs22b1005.jpg", linkedin: "https://www.linkedin.com/in/k-m-v-r-madhava-krishna-a71533262/", github: "https://github.com/Madhava0412" },
     { name: "Praneeth Devarasetty", role: "CS22B1014", bio: "Full Stack Developer skilled in React, Node.js, databases, APIs, responsive design, and performance optimization", image: "/assets/cs22b1014.jpg", linkedin: "https://www.linkedin.com/in/praneeth-devarasetty/", github: "https://github.com/praneeth622" },
     { name: "G Chaithanya Reddy", role: "CS22B1052", bio: "Networking expert, dedicated to expanding GDSC's partnerships and collaborations.", image: "/assets/cs22b1052.jpg", linkedin: "https://www.linkedin.com/in/chaitanya-reddy-gavinolla-22166b258/", github: "https://github.com/chaitanya-reddy-13" },
@@ -289,7 +289,7 @@ export default function Team() {
   const mentors = [
     { name: "Jashwanth Peddisetty", role: "Developer@Randomwalk.Ai", bio: "Developer at Randomwalk.Ai | Ex-Intern at Congruent Solutions | Web3 enthusiast | ETHForAll winner 🏆 | GDSC core member", image: "/assets/jashwanth.jpg", linkedin: "https://www.linkedin.com/in/jashwanth-peddisetty/", github: "https://github.com/jashwanth0712" },
     { name: "Vishnu Ram A V", role: "CS21B1043", bio: "Working on LLM and fine tuning", image: "/assets/cs21b1043.jpg", linkedin: "https://www.linkedin.com/in/vishnuram-av-6306b029a", github: "https://github.com/drrobertwhite" },
-    { name: "K Noineesh Reddy", role: "CS21B2023", bio: "Exploring the intersection of hardware and software in the Internet of Things.", image: "/assets/cs21b2023.jpg", linkedin: "https://linkedin.com/in/profemmagreen", github: "https://github.com/profemmagreen" },
+    { name: "K Noineesh Reddy", role: "CS21B2032", bio: "Exploring the intersection of hardware and software in the Internet of Things.", image: "/assets/cs21b2023.jpg", linkedin: "https://linkedin.com/in/profemmagreen", github: "https://github.com/profemmagreen" },
   ]
   const pics = [
     { name: "Dr. Preeth R", role: "Assistant Professor", bio: "Assistant Professor specializing in IoT, Machine Learning, Computer Vision, and Data Science research.", image: "/assets/Preeth.jpg", linkedin: "https://www.linkedin.com/in/preethr/"},
