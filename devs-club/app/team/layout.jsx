9a5a57c43380d4aea86a9a38e37ae64b1360e82d
praneeth622 <praneethdevarasetty31@gmail.com>
2025-01-12 21:33:00 +0530

    solved some errors

--- devs-club/app/team/layout.jsx
+++ devs-club/app/team/layout.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import Navbar from '../(components)/Navbar'
 import Team from './page'
 import { Footer } from '../(components)/Footer'
+import  HeroSection  from './Herosection'
 
 export default function layout() {
   return (
     <div>
         <Navbar/>
+        <HeroSection />
         <Team/>
         <Footer />
     </div>
